20e46d634e3d240ccd7a845bc7c8291275e8f629
franco <franco@matt3r.ai>
2023-01-13 13:29:40 -0800

    working with environment variables

--- handler.py
+++ handler.py
@@ -1,18 +1,20 @@
 import json
 import boto3
+import os
 
 s3 = boto3.client('s3')
+DESTINATION_BUCKET = os.environ.get('DESTINATION_BUCKET')
 
 def practiceProcessNewFile(event, context):    
-    # obtain S3 data
+
+    # obtain S3 data from the event source
     s3_bucket_start = json.loads(json.loads(event['Records'][0]['body'])['Message'])['Records'][0]['s3']['bucket']['name']
     file_name_start = json.loads(json.loads(event['Records'][0]['body'])['Message'])['Records'][0]['s3']['object']['key']
-    print("!!!1!!!", s3_bucket_start, file_name_start)
+    print(s3_bucket_start, file_name_start)
     file = s3.get_object(Bucket=s3_bucket_start, Key=file_name_start)["Body"].read()
     file_content = json.loads(file)      
     
-    # process s3 data
-    print("!!!2!!!",file_content["14176600"])
+    # process data
     sum = 0
     manualCummulative = {}
     for i in file_content.items():
@@ -21,10 +23,8 @@ def practiceProcessNewFile(event, context):
     assert len(file_content.items()) == len(manualCummulative.items()), "The cummulative array should be the same length as the input array"
     jsonFile = json.dumps(manualCummulative).encode('utf-8')
     
-    # save s3 data
-    s3_bucket_end = "franco-test-bucket2"
-    file_path = file_name_start
-    s3.put_object(Body=jsonFile, Bucket=s3_bucket_end, Key=file_path)
+    # save S3 data in 2nd bucket
+    s3.put_object(Body=jsonFile, Bucket=DESTINATION_BUCKET, Key=file_name_start)
     
     return {
         'statusCode': 200,
